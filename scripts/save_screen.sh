e99eb770fedef4862d0b3adf6cb1c146cb54aaf2
shashank-github-0001 <shashankbharadwaj0001@gmail.com>
2024-08-20 20:39:54 +0530

    routine push

--- scripts/save_screen.sh
+++ scripts/save_screen.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 
-date=$(date +'%Y-%m-%d_%H-%M-%S')
+date=$(date +'%Y-%m-%d')
 grim ~/Pictures/Screenshots/screenshot-$date.png
 notify-send -t 3000 "screenshot saved as ~/Pictures/Screenshots/screenshot-$date.png"
