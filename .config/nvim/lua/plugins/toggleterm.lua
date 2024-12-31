b3f9925f35ec35f39db8639419f73ff6711e959c
shashank-github-0001 <shashankbharadwaj0001@gmail.com>
2024-12-31 20:37:31 +0530

    Tue Dec 31 08:37:31 PM IST 2024

--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -1,29 +1,14 @@
 return {
   {
-    "akinsho/toggleterm.nvim",
-    version = "*",
-    opts = {
-      size = 20,
-      open_mapping = [[<C-\>]],
-      shade_filetypes = {},
-      shade_terminals = true,
-      shading_factor = "1",
-      start_in_insert = true,
-      insert_mappings = true,
-      persist_size = true,
-      direction = "float",
-      close_on_exit = true,
-      shell = vim.o.shell,
-      float_opts = {
-        border = "curved",
-        width = 100,
-        height = 30,
-        winblend = 3,
-      },
-    },
+    'akinsho/toggleterm.nvim',
+    version = '*',
     config = function()
-      require("toggleterm").setup({})
-      vim.keymap.set("n", "<leader>te", ":ToggleTerm<CR>")
+      require('toggleterm').setup({
+        start_in_insert = false,
+        direction = "tab",
+        size = 20,
+      })
+      vim.keymap.set('n', '<leader>te', ':ToggleTerm<CR>')
     end,
   },
 }
