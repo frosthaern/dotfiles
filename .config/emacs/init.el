63809b7ed77df2c469c33d28b8a1203e764ac757
shashank-github-0001 <shashankbharadwaj0001@gmail.com>
2025-01-26 10:56:15 +0530

    Sun Jan 26 10:56:15 AM IST 2025

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,10 +1,13 @@
 ; this is the normal shit
 (tool-bar-mode -1)
+(server-start)
 (scroll-bar-mode -1)
 (menu-bar-mode -1)
 (show-paren-mode 1)
 (setq inhibit-startup-screen t)
 (global-display-line-numbers-mode t)
+(set-fringe-mode 10)
+(setq visual-bell t)
 (setq display-line-numbers 'relative)
 (column-number-mode t)
 (set-face-attribute 'default nil :family "JetBrainsMono Nerd Font" :height 130)
